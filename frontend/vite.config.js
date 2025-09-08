2848ddb8f0cd95488c6b1282e0b0ea3aba3dd4ed
Abdulwahab Abbas <abbas@seamlesshr.com>
2025-09-09 00:58:35 +0100

    modifying the vite proxy url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        target: "https://lobby-gaming-app.onrender.com",
         changeOrigin: true,
       },
     },
